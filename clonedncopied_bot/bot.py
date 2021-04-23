15b1531bb3f49b4cf11a5d957c5b0eaba648a921
Jonathan Miller <jon@jmill.dev>
2021-04-23 12:01:42 -0400

    Bot can dump message history

--- clonedncopied_bot/bot.py
+++ clonedncopied_bot/bot.py
@@ -1,20 +1,26 @@
 import os
+from datetime import datetime
 
 import discord
 from discord import utils
 from discord.ext import commands
 from dotenv import load_dotenv
+import pandas as pd
 
-from clonedncopied_bot.constants import DOTENV_PATH
+from clonedncopied_bot import constants
 
-load_dotenv(dotenv_path=DOTENV_PATH)
-TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv(dotenv_path=constants.DOTENV_PATH)
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 
 class ClonedNCopiedBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
+        self.data_dir = constants.DATA_DIR
+        self.data_dir.mkdir(exist_ok=True)
         commands.Bot.__init__(self, command_prefix="!", intents=intents)
+
 
 class AdminCog(commands.Cog):
     def __init__(self, bot):
@@ -22,7 +28,27 @@ class AdminCog(commands.Cog):
 
     @commands.command()
     async def hi(self, ctx):
-        await ctx.send('hey')
+        await ctx.send("hey")
+
+    @commands.command()
+    async def get_message_history(self, ctx):
+        print("Getting all messages")
+        all_messages = []
+        for channel in ctx.guild.text_channels:
+            messages = await channel.history().flatten()
+            all_messages.append(messages)
+        all_messages_flat = [i for s in all_messages for i in s]
+        amfd = [{
+            "author": message.author,
+            "channel": message.channel,
+            "created_at": message.created_at,
+            "content": message.content,
+            "type": message.type
+        } for message in all_messages_flat]
+        df = pd.DataFrame(amfd)
+        df.to_csv(self.bot.data_dir / f"{ctx.guild.id}_{datetime.now().timestamp()}_messages.csv")
+        print("finished getting messages")
+
 
 def start():
     print(f"Launching bot")
--- clonedncopied_bot/constants.py
+++ clonedncopied_bot/constants.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 DOTENV_PATH = ROOT_DIR / ".env"
+DATA_DIR = ROOT_DIR / "data"
